cbe50168b690826769c1fd670b95d8caffbf78e2
Gollum <vyacheslav.glinkin@gmail.com>
2020-11-24 08:02:08 +0300

    Вторая домашка

--- Learn/script.js
+++ Learn/script.js
@@ -23,7 +23,7 @@
 
 'use strict';
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 
 const personalMovieDB = {
@@ -51,16 +51,12 @@ else {
 for (let i=0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
           b = prompt("На сколько оцените его?", "");
-            
-            
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
         } else {
-            
-            console.log('error');
+                       console.log('error');
             i--;
-        }
-    
-}
+        } 
+   }
 
 
